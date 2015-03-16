dcf4291984cbb56320b7dbf017d86c0a174747c1
Dario <brosds@gmail.com>
2015-03-16 19:15:04 +0100

    Finished Setup

--- src/main/java/firstSteps/FirstStepApplication.java
+++ src/main/java/firstSteps/FirstStepApplication.java
@@ -11,10 +11,7 @@ public class FirstStepApplication extends Application {
      */
     @Override
     public Restlet createInboundRoot() {
-        // Create a router Restlet that routes each call to a
-        // new instance of HelloWorldResource.
         Router router = new Router(getContext());
-        // Defines only one route
         router.attachDefault(HelloWorldResource.class);
 
         return router;
--- src/main/java/firstSteps/HelloWorldResource.java
+++ src/main/java/firstSteps/HelloWorldResource.java
@@ -1,18 +1,14 @@
 package firstSteps;
 
+
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
-/**
- * Resource which has only one representation.
- *
- */
+
 public class HelloWorldResource extends ServerResource {
 
     @Get
-    public String represent() {
-        System.out.println("Prova");
-        return "<html>hello, world (from the cloud!)<html>";
-    }
+    public String retrieve() {return "hello, world (from the cloud!)";}
+       
 
 }
